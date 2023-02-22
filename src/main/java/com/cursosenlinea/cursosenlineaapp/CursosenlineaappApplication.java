7bd9f37c4639b104f92b63de779eae3c4a4dbd06
linapalomo <linapalomog@hotmail.com>
2023-02-22 18:05:50 +0100

    all the project commit

---
+++ src/main/java/com/cursosenlinea/cursosenlineaapp/CursosenlineaappApplication.java
@@ -0,0 +1,13 @@
+package com.cursosenlinea.cursosenlineaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursosenlineaappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursosenlineaappApplication.class, args);
+	}
+
+}
